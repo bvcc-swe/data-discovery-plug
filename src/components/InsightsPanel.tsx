c6c189ab834410d76fd9352e48c94b5093f10458
tabbay <tabia.cannon@aa.com>
2025-11-18 18:02:23 -0600

    add: insight panel new route

--- src/components/InsightsPanel.tsx
+++ src/components/InsightsPanel.tsx
@@ -90,7 +90,7 @@ const InsightsPanel = ({
 
 	const handleGenerateInsight = () => {
 		setIsLoading(true);
-		fetch("http://localhost:4000/insight", {
+		fetch("/api/server/insight", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({
